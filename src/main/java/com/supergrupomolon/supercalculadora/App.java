dcd99d35cecf494966aec6902d94da2c56d9a303
Miguel Román Párraga <mromparb@g.educaand.es>
2022-02-07 14:27:44 +0100

    Calculo de area perimetro y apotema de poligono

--- src/main/java/com/supergrupomolon/supercalculadora/App.java
+++ src/main/java/com/supergrupomolon/supercalculadora/App.java
@@ -1,13 +1,18 @@
 package com.supergrupomolon.supercalculadora;
 
+import com.supergrupomolon.supercalculadora.Classes.Poligono;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Poligono cuadrado = new Poligono(6, 8);
+
+        System.out.println(cuadrado.area());
+        System.out.println(cuadrado.perimetro());
     }
 }
---
+++ src/main/java/com/supergrupomolon/supercalculadora/Classes/Poligono.java
@@ -0,0 +1,31 @@
+package com.supergrupomolon.supercalculadora.Classes;
+
+public class Poligono {
+    private int numLados;
+    private double lado;
+    private double apotema;
+
+    public double perimetro() {
+        double perimetro = 0;
+
+        perimetro = this.numLados * this.lado;
+
+        return perimetro;
+    }
+
+    public double area() {
+        double area = 0;
+
+        area = this.numLados * this.lado * this.apotema;
+
+        area /= 2;
+
+        return area;
+    }
+
+    public Poligono(int numLados, double lado) {
+        this.numLados = numLados;
+        this.lado = lado;
+        this.apotema = (this.lado / (2 * Math.tan(Math.toRadians(180.0 / this.numLados))));
+    }
+}
