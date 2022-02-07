51ddf2c49453d7334f91396aebf686ec2cb85906
Alvaro <alvgonzadel@gmail.com>
2022-02-07 14:10:15 +0100

    Primer y Segundo Grado terminados

--- src/main/java/com/supergrupomolon/supercalculadora/App.java
+++ src/main/java/com/supergrupomolon/supercalculadora/App.java
@@ -1,13 +1,11 @@
 package com.supergrupomolon.supercalculadora;
+import com.supergrupomolon.supercalculadora.Classes.Ecuaciones;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Ecuaciones ecuation = new Ecuaciones();
     }
 }
